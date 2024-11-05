008c777a409f357f17066175e484bc6f8fb82790
b7sh <mohammedabdelbasit500@gmail.com>
2024-11-05 21:25:27 +0000

    solving the third task errors

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-'''
+"""
 Flask app
-'''
-from flask import Flask, render_template
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
 from flask_babel import Babel
 
 
 class Config(object):
-    ''' Configuration for Babel '''
+    """
+    Configuration for Babel
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,17 +26,19 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    '''
+    """
     Select and return best language match based on supported languages
-    '''
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
-    ''' Handles / route '''
+    """
+    Handles / route
+    """
     return render_template('3-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
