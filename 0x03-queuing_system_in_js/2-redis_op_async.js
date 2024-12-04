dff2fa2c75260135fb4c8170c85f3c5c0ae83520
b7sh <mohammedabdelbasit500@gmail.com>
2024-12-04 10:19:53 +0000

    solving the third task '3. Node Redis client and async operations'

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,21 @@
+import redis from 'redis'
+import { promisify } from 'util'
+
+
+const client = redis.createClient()
+  .on('connect', () => console.log('Redis client connected to the server'))
+  .on('error', err => console.log(`Redis client not connected to the server: ${err.message}`));
+
+const addAsync = promisify(client.get).bind(client);
+
+async function setNewSchool(schoolName, value) {
+  await client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, res) => console.log(res));
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
