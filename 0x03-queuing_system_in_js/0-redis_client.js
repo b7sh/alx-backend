7ec87cc9bf9e07238773ac0e4dad016b0faa5c6b
b7sh <mohammedabdelbasit500@gmail.com>
2024-12-04 08:44:48 +0000

    solving the first task '1. Node Redis Client'

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,6 @@
+import redis from 'redis'
+
+
+const createClient = redis.createClient()
+  .on('connect', () => console.log('Redis client connected to the server'))
+  .on('error', err => console.log(`Redis client not connected to the server: ${err.message}`));
